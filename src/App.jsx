642531582d7c4db643b197232541fb36cba41e1f
badr ghanbi <bghanbi50@gmail.com>
2023-03-12 18:15:21 +0100

    add notetxt file and add buttons styls

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,9 @@
 import React from 'react'
-import Btn from './componenet/UI/Btn'
 const App = () => {
   return (
     <div >
       <h1>hello world</h1>
-      <Btn className='bg-orange hover:bg-orangePale' text="see product" />
-      <Btn className='bg-black text-white hover:bg-blackHover' text="see product" />
-      <Btn className='text-black border-[1px] py-2 hover:bg-blackHover hover:text-textWhite' text="see product" />
+     
     </div>
   )
 }
