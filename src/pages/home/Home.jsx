47fec5a9119fdde0019ba8a85f12f3bd5272ab1a
badr ghanbi <bghanbi50@gmail.com>
2023-03-13 18:42:03 +0100

    add the nav and hero section to the home page

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,29 +1,8 @@
 import React ,{useState} from 'react'
-import hamburgerLogo from ".././../../public/images/shared/tablet/icon-hamburger.svg"
-import logo from "../../../public/images/shared/desktop/logo.svg"
-import cart from "../../../public/images/shared/desktop/icon-cart.svg"
-import Overlay from '../../componenet/UI/Overlay'
-import ProductIntroCard from "../../componenet/UI/ProductIntroCard"
-
+import Nav from './Nav'
 
 export const Home = () => {
-    const [backdropIsOpen,setBackdropIsOpen] = useState(false)
-
-    const handleCardShopClicked=()=>{
-        setBackdropIsOpen(true)
-    }
-  return (
-    <header className='bg-black flex h-20 py-2 px-6 '>
-      <nav className='flex-row-between w-full'>
-      <div className='flex-row'>
-          <img className='mr-8' src={hamburgerLogo} alt='hamburger logo icon' />
-           <img src={logo}  alt='audiophile logo'  />
-      </div>
-      <img onClick={handleCardShopClicked} src={cart} alt='cart icon' />
-      </nav>
-     { backdropIsOpen &&  <Overlay setBackdropIsOpen={setBackdropIsOpen} >
-        <ProductIntroCard/>
-      </Overlay>  }
-    </header>
-  )
+   return (
+    <Nav />
+   )
 }
