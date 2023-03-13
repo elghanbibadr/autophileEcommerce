6c0fd8d874818786ab0659bfb8d882fbfc2de98a
badr ghanbi <bghanbi50@gmail.com>
2023-03-13 18:38:16 +0100

    create the backdrop componenet

--- src/componenet/UI/Overlay.jsx
+++ src/componenet/UI/Overlay.jsx
@@ -1,8 +1,9 @@
-import React from 'react'
 
 const Overlay = (props) => {
     const hanldeOverlayClicked=(e)=>{
-        console.log(e.target.classList.contains('backdrop'))
+      if (e.target.classList.contains('backdrop')){
+            props.setBackdropIsOpen(false)
+        } 
     }
   return (
     <div onClick={hanldeOverlayClicked} className='bg-lightBlack absolute backdrop inset-0 grid grid-cols-1 px-20  justify-center items-center'>
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React ,{useState} from 'react'
 import hamburgerLogo from ".././../../public/images/shared/tablet/icon-hamburger.svg"
 import logo from "../../../public/images/shared/desktop/logo.svg"
 import cart from "../../../public/images/shared/desktop/icon-cart.svg"
@@ -7,6 +7,11 @@ import ProductIntroCard from "../../componenet/UI/ProductIntroCard"
 
 
 export const Home = () => {
+    const [backdropIsOpen,setBackdropIsOpen] = useState(false)
+
+    const handleCardShopClicked=()=>{
+        setBackdropIsOpen(true)
+    }
   return (
     <header className='bg-black flex h-20 py-2 px-6 '>
       <nav className='flex-row-between w-full'>
@@ -14,11 +19,11 @@ export const Home = () => {
           <img className='mr-8' src={hamburgerLogo} alt='hamburger logo icon' />
            <img src={logo}  alt='audiophile logo'  />
       </div>
-      <img src={cart} alt='cart icon' />
+      <img onClick={handleCardShopClicked} src={cart} alt='cart icon' />
       </nav>
-      <Overlay >
+     { backdropIsOpen &&  <Overlay setBackdropIsOpen={setBackdropIsOpen} >
         <ProductIntroCard/>
-      </Overlay>
+      </Overlay>  }
     </header>
   )
 }
