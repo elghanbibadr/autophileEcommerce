8d2581f523499cbb5fa35bdcbc5b7547c257bdc1
badr ghanbi <bghanbi50@gmail.com>
2023-03-14 13:38:12 +0100

    add the close icon and make it close the productintrocontainer

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -6,7 +6,7 @@ export const Home = () => {
 
    return (
   <header >
-        <Nav setProductIntroGridContainerOpen={setProductIntroGridContainerOpen} />
+        <Nav ProductsIntroGridContainerOpen={ProductsIntroGridContainerOpen} setProductIntroGridContainerOpen={setProductIntroGridContainerOpen} />
         {ProductsIntroGridContainerOpen &&  <ProductsIntroGridContainer />}
     </header>
    )
--- src/pages/home/Nav.jsx
+++ src/pages/home/Nav.jsx
@@ -1,8 +1,10 @@
 import  React,{useState} from 'react'
 import hamburgerLogo from ".././../../public/images/shared/tablet/icon-hamburger.svg"
+import closeLogo from ".././../../public/images/shared/tablet/icon-close-menu.svg"
 import logo from "../../../public/images/shared/desktop/logo.svg"
 import cart from "../../../public/images/shared/desktop/icon-cart.svg"
 import Overlay from '../../componenet/UI/Overlay'
+
 
 const Nav = (props) => {
     const [backdropIsOpen,setBackdropIsOpen] = useState(false)
@@ -11,13 +13,18 @@ const Nav = (props) => {
     }
 
     const handleMenuHamburgerClicked=()=>{
-    props.setProductIntroGridContainerOpen(prv => !prv)
+    props.setProductIntroGridContainerOpen(true)
     
     }
+
+  const handleCloseIconClicked=()=>{
+   props.setProductIntroGridContainerOpen(false)
+  }
   return (
       <nav className='flex-row-between w-full bg-paleBlack relative  h-20 py-2 px-6 '>
       <div className='flex-row'>
-          <img onClick={handleMenuHamburgerClicked} className='mr-8' src={hamburgerLogo} alt='hamburger logo icon' />
+         { !props.ProductsIntroGridContainerOpen &&  <img onClick={handleMenuHamburgerClicked} className='mr-8 lg:hidden' src={hamburgerLogo} alt='hamburger logo icon' /> }
+        { props.ProductsIntroGridContainerOpen &&  <img onClick={handleCloseIconClicked} className='mr-8 lg:hidden' src={closeLogo} alt='close logo icon' />  }
            <img src={logo}  alt='audiophile logo'  />
       </div>
       <img onClick={handleCardShopClicked} src={cart} alt='cart icon' />
