4e62c43dac2bb52cc46f65991c8d64cfb0ae83d0
badr ghanbi <bghanbi50@gmail.com>
2023-03-14 15:19:35 +0100

    create the stupid hero section

--- src/pages/home/Hero.jsx
+++ src/pages/home/Hero.jsx
@@ -2,13 +2,15 @@ import React from 'react'
 import Btn from "../../componenet/UI/Btn"
 const Hero = () => {
   return (
-    <div  className=' text-center lg:text-left hero h-screen flex flex-col justify-center  items-center lg:items-start  bg-hero-mobile bg-center  bg-no-repeat sm:bg-hero-tablet lg:bg-hero-desktop lg:px-10 '  >
-              <h5 className='text-white opacity-50'>NEW PRODUCT</h5>
-              <h1  className='lg:w-1/3'>XX99 MARK II HEADPHONES</h1>
-              <p className=' my-4 lg:w-1/3'>
-              Experience natural, lifelike audio and exceptional build quality made for the passionate music
-              </p>
-              <Btn className='bg-orange hover:bg-orangePale' text="see product" />
+    <div  className=' lg:text-left hero h-screen flex flex-col text-center justify-center  bg-hero-mobile bg-center  bg-no-repeat sm:bg-hero-tablet lg:bg-hero-desktop lg:px-10 '  >
+              <div className=' lg:mx-2 lg:w-1/2'>
+                  <h5 className='text-white opacity-50'>NEW PRODUCT</h5>
+                  <h1  className=''>XX99 MARK II HEADPHONES</h1>
+                  <p className=' my-4 lg:w-2/3'>
+                  Experience natural, lifelike audio and exceptional build quality made for the passionate music
+                  </p>
+                  <Btn className='bg-orange hover:bg-orangePale' text="see product" />
+              </div>
     </div>
   )
 }
