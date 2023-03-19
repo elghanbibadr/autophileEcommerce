e8ea0bb1cc2c96a5e232a5e7c940cfdad8ea1ad2
badr ghanbi <bghanbi50@gmail.com>
2023-03-19 13:37:29 +0000

    more work on the footer section

--- src/pages/home/Footer.jsx
+++ src/pages/home/Footer.jsx
@@ -1,27 +1,30 @@
 import React from 'react'
 import logo from "/images/shared/desktop/logo.svg"
 import faceBookLogo from "/images/shared/desktop/icon-facebook.svg"
+import twitterLogo from "/images/shared/desktop/icon-twitter.svg"
+import instagramLogo from "/images/shared/desktop/icon-instagram.svg"
+
 const Footer = () => {
   return (
-    <footer className='bg-paleBlack h-[300px] py-10 px-10'>
-    <div>
+    <footer className='bg-paleBlack mt-40 py-10 px-10'>
+    <div className='md:flex justify-between items-center'>
     <img src={logo}  alt="audiophile logo" />
     <ul className='flex justify-between  text-sm my-8 font-bold'>
         <li>HOME</li>
-        <li>HEADPHONES</li>
+        <li className='mx-6'>HEADPHONES</li>
         <li>SPEAKERS</li>
-        <li>EARPHONES</li>
+        <li className='ml-6'>EARPHONES</li>
     </ul>
     </div>
-    <p>
+    <p className='lg:w-1/2'>
     Audiophile is an all in one stop to fulfill your audio needs. We're a small team of music lovers and sound specialists who are devoted to helping you get the most out of personal audio. Come and visit our demo facility - we’re open 7 days a week.
     </p>
     <div className='flex justify-between mt-8'>
-        <p>Copyright 2021. All Rights Reserved</p>
+        <p>Copyright 2021. All Rights <br/> Reserved</p>
         <ul className='flex  '>
             <li><img src={faceBookLogo} alt="facebook logo" /></li>
-            <li className='mx-4'><img src={faceBookLogo} alt="facebook logo" /></li>
-            <li><img src={faceBookLogo} alt="facebook logo" /></li>
+            <li className='mx-4'><img src={twitterLogo} alt="facebook logo" /></li>
+            <li><img src={instagramLogo} alt="facebook logo" /></li>
             
         </ul>
     </div>
