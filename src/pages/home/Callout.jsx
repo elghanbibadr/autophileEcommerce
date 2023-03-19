52391fbce8adf1d7939585de6cc89f0328b64384
badr ghanbi <bghanbi50@gmail.com>
2023-03-19 12:16:15 +0000

    more fix on the callout section

--- src/pages/home/Callout.jsx
+++ src/pages/home/Callout.jsx
@@ -21,9 +21,9 @@ export const Callout = () => {
 
 
   return (
-    <div className='mt-20 lg:grid lg:grid-cols-2 justify-center items-center'>
-    <img className='rounded-md col-start-2 mx-auto w-full lg:h-3/4' src={windowWidth < 700 ? bestGearMobile : windowWidth < 1024 ? bestGearTablet:bestGearDestkop } /> 
-    <div className='text-center row-start-1 lg:text-left p-8'>
+    <div className='mt-20   gap-x-6 lg:grid lg:grid-cols-2 justify-center items-center'>
+    <img className='rounded-md  lg:col-start-2  ' src={windowWidth < 700 ? bestGearMobile : windowWidth < 1024 ? bestGearTablet:bestGearDestkop } /> 
+    <div className='text-center lg:row-start-1 lg:text-left p-4'>
         <h2 className='text-black leading-[1.4] font-bold my-6'>BRINGING YOU THE <strong className='text-orange'>BEST</strong> AUDIO GEAR</h2>
         <p className='text-black'>
         Located at the heart of New York City, Audiophile is the premier store for high end headphones, earphones, speakers, and audio accessories. We have a large showroom and luxury demonstration rooms available for you to browse and experience a wide range of our products. Stop by our store to meet some of the fantastic people who make Audiophile the best place to buy your portable audio equipment.
