ed83beec4e4f5deee65b2a2e16af7d09e6da78bc
badr ghanbi <bghanbi50@gmail.com>
2023-04-04 21:04:48 +0000

    position the product containre absolute

--- src/componenet/UI/ProductsIntroGridContainer.jsx
+++ src/componenet/UI/ProductsIntroGridContainer.jsx
@@ -24,7 +24,7 @@ const ProductsIntroGridContainer = () => {
     },
 ]
   return (
-    <ul className='flex items-center lg:hidden px-6 bg-white  mt-6  h-[250px]  gap-x-4 lg:gap-x-10' >
+    <ul className='flex absolute inset-x-0  items-center lg:hidden px-6 bg-white  mt-6  h-[250px]  gap-x-4 lg:gap-x-10' >
     {data.map(({id,name,img})=>{
      return <ProductIntroCard key={id} name={name} img={img} />
     })}  
