bb4d03560c2b1055358791947f80c5e1ece64223
badr ghanbi <bghanbi50@gmail.com>
2023-04-07 17:18:34 +0000

    add the products to the speakers routes

--- src/pages/speakers/Speakers.jsx
+++ src/pages/speakers/Speakers.jsx
@@ -2,15 +2,23 @@ import React from 'react'
 import Container from '../../componenet/UI/Container'
 import RouteDescName from '../../componenet/UI/RouteDescName'
 import Callout from '../../componenet/UI/Callout'
+import ProductDesc from '../../componenet/ProductDesc'
 import Footer from '../../componenet/UI/Footer'
-const Speakers = () => {
+import { products } from '../../data/Product'
+const  Speakers = () => {
+  const speakersProduct =products.filter(product=>product.category=="speakers")
+   console.log(speakersProduct)
   return (
     <>
     <Container className='bg-paleBlack'>
      <RouteDescName routeName="speaker"  />
 </Container>
      <Container>
+     {speakersProduct.reverse().map(({id,description,categoryImage,name,new:isNewProduct},index)=>{
+        return <ProductDesc key={id} id={index} description={description} name={name} new={isNewProduct} categoryImage={categoryImage} />
+       })}
          <Callout />
+
      </Container>
      <Footer />
     </>
