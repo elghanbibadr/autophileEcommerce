e748b8d21d77f1a143b09276207d84fd14d03838
badr ghanbi <bghanbi50@gmail.com>
2023-04-07 17:01:18 +0000

    add the product of the headphone routes

--- src/componenet/ProductDesc.jsx
+++ src/componenet/ProductDesc.jsx
@@ -15,12 +15,13 @@ const ProductDesc = (props) => {
   }, []);
 
   return (
-    <div className='mt-20 gap-x-[8rem] lg:grid lg:grid-cols-2 lg:mb-[10rem] '>
+    <div className='mt-20 gap-x-[8rem] lg:grid lg:grid-cols-2 lg:mb-[7rem] '>
     <div >
       <img className='rounded-md '  src={windowWidth < 400 ? props.categoryImage.mobile : windowWidth < 1024 ? props.categoryImage.tablet:props.categoryImage.desktop}  />
        </div>
     <div className={`text-center ${props.id==1 ? 'lg:row-start-1':''}  lg:text-left lg:self-center p-4`}>
-        <h3 className='text-black leading-[1.4] font-bold my-6'>{props.name}</h3>
+    {props.new &&   <span className='text-orange tracking-[0.62rem] font-light uppercase'> new product </span> }
+        <h3 className='text-black leading-[1.4] font-bold mt-4 mb-2'>{props.name}</h3>
         <p className='text-black mb-6'>
         {props.description}
         </p>
--- src/pages/headphones/Headphones.jsx
+++ src/pages/headphones/Headphones.jsx
@@ -8,14 +8,15 @@ import { products } from '../../data/Product'
 import ProductDesc from '../../componenet/ProductDesc'
 const Headphones = () => {
   const headphonesProduct=products.filter(product=>product.category==='headphones')
+  console.log(headphonesProduct)
   return (
     <>
     <Container className='bg-paleBlack'>
      <RouteDescName routeName="Headphones"  />
 </Container>
      <Container>
-       {headphonesProduct.reverse().map(({id,description,categoryImage,name},index)=>{
-        return <ProductDesc key={id} id={index} description={description} name={name} categoryImage={categoryImage} />
+       {headphonesProduct.reverse().map(({id,description,categoryImage,name,new:isNewProduct},index)=>{
+        return <ProductDesc key={id} id={index} description={description} name={name} new={isNewProduct} categoryImage={categoryImage} />
        })}
        <Callout />
      </Container>
