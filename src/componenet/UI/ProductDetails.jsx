108dff78274e707bfac0d7dfed14ab37a85353d5
badr ghanbi <bghanbi50@gmail.com>
2023-04-11 23:26:27 +0000

    create the second section of the product details page

--- src/componenet/UI/ProductDetails.jsx
+++ src/componenet/UI/ProductDetails.jsx
@@ -29,6 +29,27 @@ function ProductDetails() {
             </div>
         </div>
       </div>
+      <div>
+        <div className='mt-20 '>
+          <h3 className='text-paleBlack mb-6'>FEATURES</h3>
+          <p className='text-black'>
+            Connect via Bluetooth or nearly any wired source. This speaker features optical, digital coaxial, USB Type-B, stereo RCA, and stereo XLR inputs, allowing you to have up to five wired source devices connected for easy switching. Improved bluetooth technology offers near lossless audio quality at up to 328ft (100m).
+          
+          Discover clear, more natural sounding highs than the competition with ZX9’s signature planar diaphragm tweeter. Equally important is its powerful room-shaking bass courtesy of a 6.5” aluminum alloy bass unit. You’ll be able to enjoy equal sound quality whether in a large room or small den. Furthermore, you will experience new sensations from old songs since it can respond to even the subtle waveforms.
+          </p>
+        </div>
+        {/* in the box div */}
+        <div className='mt-10'>
+          <h3 className='text-paleBlack mb-6'>IN THE BOX</h3>
+          <ul className='text-black'>
+            <li className='flex items-center my-2 ' ><strong className='text-orange mr-2'>2x</strong><p className='text-black '>Speaker Unit</p></li>
+            <li className='flex items-center my-2 '><strong  className='text-orange mr-2'>2x</strong><p className='text-black '>Speaker Unit</p></li>
+            <li className='flex items-center my-2 '><strong  className='text-orange mr-2'>2x</strong><p className='text-black '>Speaker Unit</p></li>
+            <li className='flex items-center my-2 '><strong  className='text-orange mr-2'>2x</strong><p className='text-black '>Speaker Unit</p></li>
+          </ul>
+        </div>
+      </div>
+
     </Container>
   );
 }
