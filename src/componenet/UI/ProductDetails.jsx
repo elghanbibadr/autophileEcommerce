f1188f061cb6f208b350f28c41dfedc115cbce04
badr ghanbi <bghanbi50@gmail.com>
2023-04-12 15:55:00 +0000

    make the li component of the icludes item list dynamique

--- src/componenet/UI/ProductDetails.jsx
+++ src/componenet/UI/ProductDetails.jsx
@@ -43,10 +43,10 @@ function ProductDetails() {
         <div className='justify-self-center mt-10 md:mt-0' >
           <h3 className='text-paleBlack mb-6'>IN THE BOX</h3>
           <ul className='text-black'>
-            <li className='flex items-center my-2 ' ><strong className='text-orange mr-4'>2x</strong><p className='text-black '>Speaker Unit</p></li>
-            <li className='flex items-center my-2 '><strong  className='text-orange mr-4'>2x</strong><p className='text-black '>Speaker Unit</p></li>
-            <li className='flex items-center my-2 '><strong  className='text-orange mr-4'>2x</strong><p className='text-black '>Speaker Unit</p></li>
-            <li className='flex items-center my-2 '><strong  className='text-orange mr-4'>2x</strong><p className='text-black '>Speaker Unit</p></li>
+                {currentVisibleProduct.includedItems.map(({quantity,item},index)=>{
+              return   <li key={index} className='flex items-center my-2 ' ><strong className='text-orange mr-4'>{quantity}x</strong><p className='text-black '>{item}</p></li>
+          })}
+            
           </ul>
         </div>
       </div>
