ac998b1365827f49a2d91d0f68805323fe88e06c
badr ghanbi <bghanbi50@gmail.com>
2023-04-12 16:15:34 +0000

    attaced even to increase and decrease icons that change the ittem quantity value

--- src/componenet/UI/IncreaseQuantityBox.jsx
+++ src/componenet/UI/IncreaseQuantityBox.jsx
@@ -1,11 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const IncreaseQuantityBox = () => {
-  return (
+    const [itemQuantity, setItemQuantity] =useState(1)
+  const handleIncreaseBtnClicked=()=>setItemQuantity(prv => ++prv)
+  const handleDecreaseBtnClicked=()=>{
+    if (itemQuantity==1)return;
+    setItemQuantity(prv => --prv)
+  }
+    return (
     <div className='bg-lightGray w-[140px] flex justify-between items-center px-5 py-3 '>
-    <span className='text-paleBlack cursor-pointer hover:text-orange ' >+</span>
-    <span className='text-black'>1</span>
-    <span className='text-paleBlack cursor-pointer hover:text-orange'>-</span>
+    <span onClick={handleIncreaseBtnClicked} className='text-paleBlack cursor-pointer hover:text-orange ' >+</span>
+    <span className='text-black'>{itemQuantity}</span>
+    <span onClick={handleDecreaseBtnClicked} className='text-paleBlack cursor-pointer hover:text-orange'>-</span>
 </div>
   )
 }
