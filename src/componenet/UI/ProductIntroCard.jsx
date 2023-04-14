765764193fb3d7817a55ccadda12b353e61f14bf
badr ghanbi <bghanbi50@gmail.com>
2023-04-14 17:20:35 +0000

    make the button on the home page dynamic

--- src/componenet/UI/ProductIntroCard.jsx
+++ src/componenet/UI/ProductIntroCard.jsx
@@ -17,9 +17,9 @@ const ProductIntroCard = (props) => {
     <Link to={`/${props.name}`}   onMouseOver={handleMouseOver}   onMouseOut={handleMouseOut}  className='bg-lightGray rounded-md relative h-40 flex my-10 justify-center  flex-col items-center w-full'>
         <img className='h-32 absolute  bottom-16 ' src={props.img} />
         <div className='mt-20 text-center  uppercase'>
-          <h4 className='text-black  tracking-[0.1rem] mb-4	 font-semibold'>{props.name}</h4>
+          <h4 className='text-black  tracking-[0.1rem]  mb-4  font-semibold'>{props.name}</h4>
           <div className='flex  items-center justify-center mb-4'>
-            <a className={` text-[0.8rem] font-medium mr-2 ${isHovered ? "text-orange":"text-[#00000080]"}`}>shop</a>
+            <a className={`text-[0.8rem] font-medium mr-2 ${isHovered ? "text-orange":"text-[#00000080]"}`}>shop</a>
             <img src={arrowRight} alt='arrow right icon' />
           </div>
         </div>
--- src/pages/home/Hero.jsx
+++ src/pages/home/Hero.jsx
@@ -1,15 +1,18 @@
 import React from 'react'
 import Btn from "../../componenet/UI/Btn"
+import { Link } from 'react-router-dom'
 const Hero = () => {
   return (
     <div  className=' lg:text-left hero h-screen flex flex-col text-center justify-center  bg-hero-mobile bg-center  bg-no-repeat sm:bg-hero-tablet lg:bg-hero-desktop  '  >
               <div className='  lg:w-1/2'>
                   <h5 className='text-white opacity-50'>NEW PRODUCT</h5>
-                  <h1  className=''>XX99 MARK II HEADPHONES</h1>
+                  <h1>XX99 MARK II HEADPHONES</h1>
                   <p className=' my-4 lg:w-2/3'>
                   Experience natural, lifelike audio and exceptional build quality made for the passionate music
                   </p>
-                  <Btn className='bg-orange hover:bg-orangePale' text="see product" />
+                  <Link to='headphones/xx99-mark-two-headphones'>
+                    <Btn className='bg-orange hover:bg-orangePale' text="see product" />
+                  </Link>
               </div>
     </div>
   )
