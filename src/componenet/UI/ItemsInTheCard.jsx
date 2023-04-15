8aeac6ac8b3e8fb856ba9bc0b4c26f9fd2ad7798
badr ghanbi <bghanbi50@gmail.com>
2023-04-15 14:49:08 +0000

    add the checkout button to the cart component

--- src/componenet/UI/ItemsInTheCard.jsx
+++ src/componenet/UI/ItemsInTheCard.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import earphoneImg from "../../../public/images/cart/image-yx1-earphones.jpg"
 import IncreaseQuantityBox from './IncreaseQuantityBox'
+import Btn from './Btn'
 const ItemsInTheCard = () => {
   return (
     <div className='px-' >
         <div className='flex  justify-between items-center'>
         <h4 className='text-black'>Cart (1)</h4>
-        <p className='text-paleBlack'>RemoveAll</p> 
+        <p className='text-paleBlack underline'>RemoveAll</p> 
         </div>
         <div className='flex  justify-between items-center'>
           <div className='flex items-center  my-6'>
@@ -18,6 +19,12 @@ const ItemsInTheCard = () => {
           </div>
           <IncreaseQuantityBox/>
         </div>
+        <div className='flex justify-between'>
+          <p className='text-paleBlack font-semibold'>TOTAL</p>
+          <h4 className='text-black'>$3,594</h4>
+        </div>
+        <Btn className='bg-orange w-full mt-10 text-white' text="CHECKOUT"/>
+
     </div>
   )
 }
