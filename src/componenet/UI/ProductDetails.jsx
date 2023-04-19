4565b23a8e2d7c5fdf1d6f0a2c88b82c6918f7cc
badr ghanbi <bghanbi50@gmail.com>
2023-04-19 14:51:24 +0000

    create a resuable input with label componenet

--- src/componenet/UI/ProductDetails.jsx
+++ src/componenet/UI/ProductDetails.jsx
@@ -102,7 +102,7 @@ function ProductDetails() {
          <div className='mt-40 max-w-[1000px] mx-auto'>
            <h3 className='text-paleBlack text-center'>YOU MAY ALSO LIKE</h3>
            <div className=' lg:grid lg:grid-cols-3 lg:gap-10'>
-              {currentVisibleProduct.others.map(({image,name,slug,category})=>{
+              {currentVisibleProduct.others.map(({image,name,slug})=>{
            
              return <div className='text-center my-10'> <img className='' src={ windowWidth < 960 ? image.mobile : image.desktop} alt="other category product image"  />
                   <h3 className='text-paleBlack leading-[1.4] font-bold my-8 text-3xl'>{name}</h3>
--- src/pages/checkout/Checkout.jsx
+++ src/pages/checkout/Checkout.jsx
@@ -14,7 +14,12 @@ const Checkout = () => {
      <Link onClick={handleBackClick} className='text-lightBlack  font-semibold'>Go Back</Link>
     {/* checkkout info */}
      <div className='bg-white h-screen p-8 rounded-md mt-8 '>
-      <h6 className='text-orange uppercase text-[1rem] font-semibold'>billing details</h6>
+      <h3 className='text-black mb-8 font-bold'>CHECKOUT</h3>
+      <h6 className='text-orange uppercase mb-8 text-[1rem] font-semibold'>billing details</h6>
+      <div  >
+        <label >Name</label>
+        <input   type='text' placeholder='Alexei Ward' />
+      </div>
      </div>
     </Container>
   )
