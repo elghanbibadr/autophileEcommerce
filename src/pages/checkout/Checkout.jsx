2c4ff043455519b655dc8dbb535e166992d01864
badr ghanbi <bghanbi50@gmail.com>
2023-04-19 15:37:47 +0000

    remove the paginations on the number input

--- src/pages/checkout/Checkout.jsx
+++ src/pages/checkout/Checkout.jsx
@@ -15,11 +15,47 @@ const Checkout = () => {
     {/* checkkout info */}
      <div className='bg-white h-screen p-8 rounded-md mt-8 '>
       <h3 className='text-black mb-8 font-bold'>CHECKOUT</h3>
-      <h6 className='text-orange uppercase mb-8 text-[1rem] font-semibold'>billing details</h6>
-      <div  >
-        <label >Name</label>
-        <input   type='text' placeholder='Alexei Ward' />
+      <h6 className='text-orange text-sm uppercase mb-8  font-semibold'>billing details</h6>
+        {/* billing details grid*/}
+      <div className="grid grid-cols-2 gap-3">
+        <div  >
+          <label >Name</label>
+          <input type='text' placeholder='Alexei Ward' />
+        </div>
+        <div  >
+          <label >Email Address</label>
+          <input type='email' placeholder='alexei@mail.com' />
+        </div>
+        <div  >
+          <label >Phone Number</label>
+          <input type='text' placeholder='+1 202-555-0136' />
+        </div>
       </div>
+      <h6 className='text-orange text-sm uppercase mt-9 mb-5  font-bold'>shipping info</h6>
+
+        {/*  shipping info grid*/}
+      <div className="grid grid-cols-2 gap-3">
+        <div className='col-span-2 mb-2 ' >
+          <label >Your Adress</label>
+          <input type='text' placeholder='1137 Williams Avenue' />
+        </div>
+        <div>
+          <label>ZIP code</label>
+          <input type='number' placeholder='10001' />
+        </div>
+        <div>
+          <label >City</label>
+          <input type='text' placeholder='New York' />
+        </div>
+        <div>
+          <label >Country</label>
+          <input type='text' placeholder='United States' />
+        </div>
+      </div>
+        {/*  shipping info grid*/}
+        <h6 className='text-orange text-sm uppercase mt-9 mb-5  font-bold'>PAYMENT DETAILS</h6>
+        <label>Payment Method</label>
+        <input type='checkbox' />
      </div>
     </Container>
   )
