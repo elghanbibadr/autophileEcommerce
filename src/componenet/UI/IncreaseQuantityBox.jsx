4224ca6327204d294e81b02d41b8ed04bf36a732
badr ghanbi <bghanbi50@gmail.com>
2023-04-23 18:04:22 +0100

    fix increase quantity box alignment issue

--- src/componenet/UI/IncreaseQuantityBox.jsx
+++ src/componenet/UI/IncreaseQuantityBox.jsx
@@ -8,7 +8,7 @@ const IncreaseQuantityBox = ({itemNumber,handleItemQuantityChanged}) => {
   });  const handleIncreaseBtnClicked=()=>{
     setItemQuantity(prv => prv + 1 )
   }
-  console.log(itemQuantity)
+
   handleItemQuantityChanged(itemQuantity)
   const handleDecreaseBtnClicked=()=>{
     if (itemQuantity==1)return;
@@ -16,7 +16,7 @@ const IncreaseQuantityBox = ({itemNumber,handleItemQuantityChanged}) => {
 
   }
     return (
-    <div className='bg-lightGray w-[90px] flex justify-between items-center px-4 py-1 '>
+    <div className='bg-lightGray w-[120px] flex justify-between  items-center self-stretch px-4 py-1 '>
     <span onClick={handleIncreaseBtnClicked} className='text-paleBlack cursor-pointer hover:text-orange ' >+</span>
     <span className='text-black font-semibold text-xs'>{itemQuantity}</span>
     <span onClick={handleDecreaseBtnClicked} className='text-paleBlack cursor-pointer hover:text-orange'>-</span>
