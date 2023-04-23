9e6efe99b2145576319f8c473de527f22de422f5
badr ghanbi <bghanbi50@gmail.com>
2023-04-23 16:47:41 +0100

    code splitting routes loading

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,23 @@
-import React,{useState} from 'react'
+import React,{useState,lazy,Suspense} from 'react'
 import { Home } from './pages/home/Home'
-import { Route,Routes } from 'react-router-dom'
-import Headphones from './pages/headphones/Headphones'
+import { Route,Routes,useLocation } from 'react-router-dom'
 import Nav from './pages/home/Nav'
-import Earphones from './pages/earphone/Earphones'
-import Speakers from './pages/speakers/Speakers'
 import ProductDetails from './componenet/UI/ProductDetails'
 import ProductsIntroGridContainer from './componenet/UI/ProductsIntroGridContainer'
-import Checkout from './pages/checkout/Checkout'
 import Callout from './componenet/UI/Callout'
 import Footer from './componenet/UI/Footer'
 
+
+const Headphones=lazy(()=>import ('./pages/headphones/Headphones'));
+const Speakers=lazy(()=>import ('./pages/speakers/Speakers'));
+const Earphones=lazy(()=>import ('./pages/earphone/Earphones'));
+const Checkout=lazy(()=>import ('./pages/checkout/Checkout'));
+
+
 const App = () => {
   const [ProductsIntroGridContainerOpen,setProductIntroGridContainerOpen] = useState(false)
+  const location = useLocation();
+  const isCheckoutRoute = location.pathname === '/checkout';
 
   return (
   
@@ -25,18 +30,20 @@ const App = () => {
             </div>
           }
             </div>
-    <Routes>
-    <Route path="/" element={<Home />} />
-    <Route path="/headphones" element={<Headphones />}/>
-    <Route path="/headphones/:slug" element={<ProductDetails />}/>
-     <Route path="/speakers" element={<Speakers />} />
-     <Route path="/speakers/:slug" element={<ProductDetails />}/>
-    <Route path="/earphones" element={<Earphones />} /> 
-    <Route path="/earphones/:slug" element={<ProductDetails />}/>
-    <Route path="/checkout" element={<Checkout />}/>
-  </Routes>
-  <Callout />
-  <Footer />
+    <Suspense fallback={<h1 className='text-black text-[6rem]'>loading ...</h1>}>
+      <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/headphones" element={<Headphones />}/>
+      <Route path="/headphones/:slug" element={<ProductDetails />}/>
+       <Route path="/speakers" element={<Speakers />} />
+       <Route path="/speakers/:slug" element={<ProductDetails />}/>
+      <Route path="/earphones" element={<Earphones />} />
+      <Route path="/earphones/:slug" element={<ProductDetails />}/>
+      <Route path="/checkout" element={<Checkout />}/>
+        </Routes>
+    </Suspense>
+    {!isCheckoutRoute && <Callout />}
+      {!isCheckoutRoute && <Footer />}
 
   </>
   )
