933d704d04f80865ac0d2a0ef3f83518f86ffb56
badr ghanbi <bghanbi50@gmail.com>
2023-04-26 10:58:07 +0100

    fix some responsivness issues on the call to action componenet on the home page

--- src/pages/home/Cta.jsx
+++ src/pages/home/Cta.jsx
@@ -5,12 +5,12 @@ import Btn from "../../componenet/UI/Btn"
 
 const Cta = () => {
   return (
-    <div className='bg-orange my-14 cta rounded-md flex  flex-col items-center text-center justify-between p-6 lg:grid lg:grid-cols-2 lg:text-left lg:bg-left'>
+    <div className='bg-orange my-14 cta rounded-md flex-col-between text-center  p-6 lg:flex-row  lg:text-left lg:bg-left'>
       <img className='lg:hidden' src={zx9speakerimage} alt='zx9 speaker image' />
-      <img className='hidden  lg:block w-full p-10 scale-90 ' src={zx9speakerimageDesktop} alt='zx9 speaker image' />
+      <img className='hidden  lg:block w-[55%] p-10 scale-90 ' src={zx9speakerimageDesktop} alt='zx9 speaker image' />
       <div className='my-8 mx-auto'>
-        <h2>ZX9 SPEAKER</h2>
-        <p className='my-4 lg:w-1/2 '>Upgrade to premium speakers that are phenomenally built to deliver truly remarkable sound.</p>
+        <h2 className='font-bold leading-[1.3]'>ZX9 <br/> SPEAKER</h2>
+        <p className='my-4 '>Upgrade to premium speakers that are phenomenally built to deliver truly remarkable sound.</p>
         <Link to='speakers/zx9-speaker'>
           <Btn className='bg-black text-white  hover:bg-blackHover' text="see product" />
         </Link>
